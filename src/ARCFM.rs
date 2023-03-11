26e9304096c973d46c551e35fa53c421ffc7659f
Sam Voss <samvoss69@gmail.com>
2023-03-11 22:29:23 +1100

    working table

--- src/ARCFM.rs
+++ src/ARCFM.rs
@@ -1,13 +1,17 @@
+use std::io::{self, Stdout};
+
+use tui::Frame;
+use tui::backend::CrosstermBackend;
 use tui::buffer::Buffer;
 use tui::layout::{Constraint, Layout, Rect, Direction};
-use tui::style::Style;
-use tui::widgets::{Block, Borders, Cell, Widget, TableState, StatefulWidget, Row, Table};
-use tui::text::Text;
+use tui::style::{Style, Color};
+use tui::symbols::block;
+use tui::widgets::{Block, Borders, Cell, Widget, TableState, StatefulWidget, Row, Table, Paragraph};
+use tui::text::{Text, Span, Spans};
 use unicode_width::UnicodeWidthStr;
 
 #[derive(Clone,Copy)]
-pub struct AbsorberRodControlAndFuelMonitoring {
-    reactor_fuel_rod_matrix: [[FuelRod; 5];5],
+pub struct ARCFMMENU {
     pull_rods: bool,
     insert_rods: bool,
     hold_rods: bool,
@@ -17,20 +21,18 @@ pub struct AbsorberRodControlAndFuelMonitoring {
     centre_core_only: bool,
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 pub struct FuelRod {
     absorber_rod_position: f32,
     fuel_temperature: f32,
     thermal_power_output: f32,
-    pull_rod: bool,
     insert_rod: bool,
 }
 
 
-impl Default for AbsorberRodControlAndFuelMonitoring {
+impl Default for ARCFMMENU {
     fn default() -> Self {
         Self {
-            reactor_fuel_rod_matrix: [[FuelRod::default(); 5]; 5],
             pull_rods: false,
             insert_rods: false,
             hold_rods: false,
@@ -47,7 +49,6 @@ impl Default for FuelRod {
             absorber_rod_position: 0.0,
             fuel_temperature: 0.0,
             thermal_power_output: 0.0,
-            pull_rod: false,
             insert_rod: false,
         }
     }
@@ -56,7 +57,7 @@ impl Default for FuelRod {
 pub fn fuel_rod_table_row(starting_number: i32, width: i32) -> Row<'static> {
     let mut cell_vec = Vec::new();
     //println!("starting number: {}", starting_number+1);
-    for i in starting_number+1..starting_number+width+1 {
+    for i in starting_number..starting_number+width+1 {
         //println!("cell number: {}", i);
         let text = Text::from(i.to_string());
         let cell = Cell::from(text).style(tui::style::Style::default().fg(tui::style::Color::Yellow));
@@ -66,15 +67,41 @@ pub fn fuel_rod_table_row(starting_number: i32, width: i32) -> Row<'static> {
     
 }
 
-pub fn fuel_rod_table(width: i32, height: i32) -> Table<'static>{
-    let mut row_vec = Vec::new();
-    for i in 1..height {
-        //println!("height: {}", i);
-        //println!("{} * {}: {}", i, height, i*height);
-        row_vec.push(fuel_rod_table_row(i*height, width));
+pub fn fuel_rod_table(width: i32, height: i32, layout: Rect, frame: &mut Frame<CrosstermBackend<Stdout>>) {
+    let column_constraints = std::iter::repeat(Constraint::Percentage((100 / width) as u16))
+        .take((width) as usize)
+        .collect::<Vec<_>>();
+    let row_constraints = std::iter::repeat(Constraint::Percentage((100 / height) as u16))
+        .take((height) as usize)
+        .collect::<Vec<_>>();
+
+    let row_rects = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(row_constraints)
+        .margin(1)
+        .split(layout);
+
+    let fuel_rods = vec![vec![FuelRod::default(); 5]; 5];
+    for i in 0..height {
+        let column_rects = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints(column_constraints.clone())
+            .split(row_rects[i as usize]);
+        for j in 0..width {
+/*             let text = vec![
+                //Spans::from(Span::from(format!("{}", (i)*width+j+1)))
+                // every struct value from the fuel rod
+                Spans::from(Span::from(format!("{:?}", fuel_rods[i as usize][j as usize].absorber_rod_position))),
+                Spans::from(Span::from(format!("{:?}", fuel_rods[i as usize][j as usize].fuel_temperature))),
+                Spans::from(Span::from(format!("{:?}", fuel_rods[i as usize][j as usize].thermal_power_output))),
+                Spans::from(Span::from(format!("{:?}", fuel_rods[i as usize][j as usize].insert_rod))),
+            ]; */
+            let text = Text::from(format!("FR: {}, POS: {}, °C: {}. TMO: {}, IN: {}", i*width+j+1,fuel_rods[i as usize][j as usize].absorber_rod_position, fuel_rods[i as usize][j as usize].fuel_temperature, fuel_rods[i as usize][j as usize].thermal_power_output, fuel_rods[i as usize][j as usize].insert_rod));
+            let cell_text = Paragraph::new(text)
+                .block(Block::default().borders(Borders::NONE)
+                .style(Style::default().bg(Color::Black)));
+            frame.render_widget(cell_text, column_rects[j as usize]);
+        }
     }
-    Table::new(row_vec).block(Block::default().title("Fuel Rods").borders(Borders::ALL)).column_spacing(100/width as u16)
-    .style(
-        tui::style::Style::default().fg(tui::style::Color::Yellow),
-    )
+
 }   
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::{io::{self, Write}, thread, time::Duration, vec};
 use tui::{
     backend::CrosstermBackend,
-    widgets::{Block, Borders, Table},
+    widgets::{Block, Borders, Table, Paragraph},
     layout::{Layout, Constraint, Direction},
-    Terminal, buffer::Buffer,
+    Terminal, buffer::Buffer, symbols::block,
 };
 use crossterm::{
     event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
@@ -11,10 +11,9 @@ use crossterm::{
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
 // import local modules
-use crate::{generic_structs::Menus, ARCFM::fuel_rod_table};
+use crate::{ARCFM::fuel_rod_table};
 use crate::ARCFM::fuel_rod_table_row;
 
-mod generic_structs;
 #[allow(non_snake_case)]
 mod ARCFM;
 #[warn(non_snake_case)]
@@ -26,27 +25,27 @@ fn main() -> Result<(), io::Error>{
     execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
-
+    let mut command_text = String::new();
     // Create a new menu
-    let mut menu = Menus::default();
     
     loop {
-            
         
-        terminal.draw(|f| {
-            let size = f.size();
+        let block = Block::default().title("Fuel Rods").borders(Borders::ALL);    
+        
+        terminal.draw(|frame| {
+            let terminal_rect = frame.size();
             let chunks = Layout::default()
                 .direction(Direction::Vertical)
                 .margin(1)
                 .constraints(
                     [
+                        Constraint::Percentage(10),
                         Constraint::Percentage(50),
                         Constraint::Percentage(50),
                     ]
                     .as_ref(),
                 )
-                .split(size);
-            let block = Block::default().borders(Borders::ALL);
+                .split(terminal_rect);
             // render a 5x5 grid of fuel rods with a table
 
             let table = Table::new(
@@ -60,6 +59,7 @@ fn main() -> Result<(), io::Error>{
 
                 ],
 
+                
             ).block(Block::default().title("Fuel Rods").borders(Borders::ALL)).widths(
                 &[
                     Constraint::Percentage(20),
@@ -71,27 +71,40 @@ fn main() -> Result<(), io::Error>{
             ).style(
                 tui::style::Style::default().fg(tui::style::Color::Yellow),
             );
-            
-            block.inner(f.size());
+        
+            //fuel_rod_table(5, 5); 
+            let tui_command_text = Paragraph::new(command_text.as_str()).block(Block::default().title("Command").borders(Borders::ALL));
+            //let block_3 = Block::default().title("Command").borders(Borders::ALL);
+            frame.render_widget(tui_command_text, chunks[0]);
+            frame.render_widget(block, chunks[1]);
 
-            //fuel_rod_table(5, 5);
-            f.render_widget(block, chunks[0]);
-            f.render_widget(table, chunks[1]);
+            fuel_rod_table(5, 5, chunks[1], frame);
+            let block_2 = Block::default().title("Menu").borders(Borders::ALL);
+            frame.render_widget(block_2, chunks[2]);
  
             
         })?;
-        thread::sleep(Duration::from_millis(5000));
+        // break when ctrl Q is pressed
+        match event::read()? {
+            Event::Key(event) => {
+                if event.code == KeyCode::Char('Q') && event.modifiers == event::KeyModifiers::SHIFT {
+                    break;
+                } else {
+                    command_text = format!("{}{}", command_text, format!("{:?}", event.code).strip_prefix("Char('").unwrap().strip_suffix("')").unwrap());
+                    println!("{}", command_text);
+                }
+            },
+            Event::FocusGained => (),
+            Event::FocusLost => (),
+            Event::Mouse(_) => (),
+            Event::Paste(_) => (),
+            Event::Resize(_, _) => (),
 
-        // restore terminal
-        disable_raw_mode()?;
-        execute!(
-            terminal.backend_mut(),
-            LeaveAlternateScreen,
-            DisableMouseCapture
-        )?;
-        terminal.show_cursor()?;
-
+        }
+        
+        
     }
+    Ok(())
     
     
 }
